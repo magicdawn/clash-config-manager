648c54b8e75b8d0fdeb2f5f615ac05fef8740b6b
magicdawn <magicdawn@qq.com>
2020-09-18 01:14:01 +0800

    chore: clean up

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,19 +1,10 @@
 import React, {useState, useCallback} from 'react'
-import {Button, DatePicker, version, Layout, Menu, Steps} from 'antd'
-import {MailOutlined, AppstoreOutlined, SettingOutlined} from '@ant-design/icons'
-
-const {Header, Footer, Sider, Content} = Layout
-const {SubMenu} = Menu
-const {Step} = Steps
+import {Button} from 'antd'
 
 export default function Home() {
   return (
-    <div className='home'>
-      <Steps>
-        <Step title='第一步' />
-        <Step title='第二步' />
-        <Step title='第三步' />
-      </Steps>
+    <div className='home' style={{padding: 20}}>
+      <h1>Enjoy</h1>
     </div>
   )
 }
