e3a6cd51b2e15f30383d3f3ee01ad676f2b5fb49
magicdawn <magicdawn@qq.com>
2020-09-19 19:07:43 +0800

    chore: fix script

--- script-release.js
+++ script-release.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
+/* eslint camelcase: off */
 
 const {execSync} = require('child_process')
-
 const sh = (cmd) => {
   console.log('[exec]: %s', cmd)
   execSync(cmd, {stdio: 'inherit'})
@@ -11,12 +11,19 @@ const sh = (cmd) => {
 // 2. npm version patch or minor
 
 // 3.push
-sh('gp origin --all && gp origin --tags')
+sh('git push origin --all && git push origin --tags')
 
 // 4.build
 sh('yarn dist:mac')
 
 // 5.release
 const {version} = require('./package.json')
+
+// need proxy
+Object.assign(process.env, {
+  https_proxy: 'http://127.0.0.1:7890',
+  http_proxy: 'http://127.0.0.1:7890',
+  all_proxy: 'socks5://127.0.0.1:7890',
+})
 sh(`gh release create v${version} -F CHANGELOG.md`)
 sh(`gh release upload v${version} ./dist/clash-config-manager-${version}.dmg`)
