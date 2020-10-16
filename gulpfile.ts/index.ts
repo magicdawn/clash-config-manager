b19228d2b93b6783233520804aee3f4b1ef736b0
magicdawn <magicdawn@qq.com>
2020-10-16 20:10:22 +0800

    chore: add gulp task builder

--- gulpfile.ts/index.ts
+++ gulpfile.ts/index.ts
@@ -1,7 +1,16 @@
 import log from 'fancy-log'
+import {task} from './util'
 
 export default async function () {
   log('default')
 }
 
 export {default as release} from './release'
+
+export const buildUi = task({
+  displayName: 'build:ui',
+  description: 'build 前端代码',
+  async run() {
+    log('build:ui')
+  },
+})
