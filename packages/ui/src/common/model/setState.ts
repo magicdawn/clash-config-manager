3870ed40a456cac72bd69a1c2dca5f53dcf76942
magicdawn <magicdawn@qq.com>
2022-04-11 02:21:30 +0800

    chore: more types

--- packages/ui/src/common/model/setState.ts
+++ packages/ui/src/common/model/setState.ts
@@ -1,10 +1,12 @@
 import { Action, action, State } from 'easy-peasy'
 
+type PlainObject = Record<string, unknown>
+
 export type SetStatePayload<T> = Partial<T> | ((state: T) => T | undefined | void)
 
-export type SetState<IModel extends object> = Action<IModel, SetStatePayload<State<IModel>>>
+export type SetState<IModel extends PlainObject> = Action<IModel, SetStatePayload<State<IModel>>>
 
-export const setStateFactory = <IModel extends object>() => {
+export const setStateFactory = <IModel extends PlainObject>() => {
   return action<IModel, SetStatePayload<State<IModel>>>((state, payload) => {
     if (typeof payload === 'object') {
       Object.assign(state, payload)
