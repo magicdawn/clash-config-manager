6a0d59fd531b1b95ad824eeebed80d0ff7db6a69
magicdawn <magicdawn@qq.com>
2022-05-14 00:02:23 +0800

    chore: more with deps

--- gulpfile.ts/index.ts
+++ gulpfile.ts/index.ts
@@ -1,16 +1,7 @@
 import log from 'fancy-log'
-import { TaskFunctionParams } from 'undertaker'
 
 export default async function () {
   log('default')
 }
 
 export { default as release } from './release'
-
-Object.assign(buildUi, {
-  displayName: 'build:ui',
-  description: 'build 前端代码',
-} as TaskFunctionParams)
-export async function buildUi() {
-  log('build:ui')
-}
