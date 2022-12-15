6a875dc2244f0e4f917a4d81dd7031cc7231529e
Tao <magicdawn@qq.com>
2022-12-15 21:01:44 +0800

    chore: 缩减 monaco editor 使用

--- packages/ui/src/common/monaco/setup.ts
+++ packages/ui/src/common/monaco/setup.ts
@@ -5,29 +5,18 @@
 
 // react-monaco-editor => momaco-editor/esm/vs/editor/editor.api.js, 而此 entry 只包含基础编辑功能
 // 使用 import monaco-editor => monaco-editor/esm/vs/editor.main.js, 导入全部功能(包含其他 language, 查找,替换模块)
-// 也可以更为详细的定制, 这里简单包含所有功能
-import 'monaco-editor'
+
+// 简单包含所有功能
+// import 'monaco-editor'
+
+// 也可以更为详细的定制
+import 'monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution'
+import 'monaco-editor/esm/vs/editor/edcore.main'
 
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
 self.MonacoEnvironment = {
   getWorker(_, label) {
-    if (label === 'json') {
-      return new jsonWorker()
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker()
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker()
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker()
-    }
     return new editorWorker()
   },
 }
