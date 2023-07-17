bf5796c5b573758e360d97291a288232a5e39663
Tao <magicdawn@qq.com>
2023-07-17 21:49:20 +0800

    fix: tsup build replace process.env.NODE_ENV to production

--- packages/main/tsup.config.ts
+++ packages/main/tsup.config.ts
@@ -1,19 +1,22 @@
 import path from 'path'
 import { defineConfig } from 'tsup'
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV || 'development'
 const REPO_ROOT = path.join(__dirname, '../../')
 
 export default defineConfig({
   entry: ['./src/index.ts'],
   format: 'cjs',
   outDir: path.join(REPO_ROOT, `bundle/${env}/main/`),
-  platform: 'node',
 
+  platform: 'node',
   // TODO: get node version based on electron version
   // Using: Node.js v18.15.0 and Electron.js v25.3.0
   target: 'node18',
+
+  env: {
+    NODE_ENV: env,
+  },
 
   // NOTE: 此处 external & noExternal 只是输入给 esbuild.options({ plugins: [externalPlugin] })
   // 无法做到, bundle any dep, except `electron`
